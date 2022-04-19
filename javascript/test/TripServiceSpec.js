94ad1559876b3dbf9bb97e5423956270d51ef02e
hanc.han <chihchun.han@gmail.com>
2022-04-19 14:46:10 +0800

    定義測試案例

--- javascript/test/TripServiceSpec.js
+++ javascript/test/TripServiceSpec.js
@@ -5,8 +5,16 @@ let TripService = require('../src/TripService');
 
 describe('TripService', () => {
 
-    it('should... ', () => {
-        assert.equal(2+2, 5);
+    xit('should_Throw_Exception_When_User_Is_Not_LoggedIn', () => {
+        assert.throws();
+    });
+
+    xit('should_Not_Return_Trips_When_Logged_User_Are_Not_Friend', () => {
+        assert.equal();
+    });
+
+    xit('should_Return_Trips_When_Logged_User_Are_Friend', () => {
+        assert.equal();
     });
 
 });
