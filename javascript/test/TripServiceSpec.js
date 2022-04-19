a2970ee283f391e3c450d763846992f63ed95930
hanc.han <chihchun.han@gmail.com>
2022-04-19 16:43:48 +0800

    測試案例重新排版

--- javascript/test/TripServiceSpec.js
+++ javascript/test/TripServiceSpec.js
@@ -3,64 +3,42 @@
 let assert = require('assert');
 let TripService = require('../src/TripService');
 let User = require('../src/User');
-let UserSession = require('../src/UserSession');
-let TripDAO = require('../src/TripDAO')
 
 describe('TripService', () => {
-
+    const UserSession = {}
+    const tripService = new TripService()
     it('should_Throw_Exception_When_User_Is_Not_LoggedIn', () => {
-        const UserSession = {
-            getLoggedUser() {
-                return null
-            }
-        }
-        const tripService = new TripService()
+        UserSession.getLoggedUser = () => null
         assert.throws(() => tripService.getTripsByUser(null, UserSession), Error, 'User not logged in.');
-    });
 
+    });
+    class Trip { }
+    const TripDAO = {
+        findTripsByUser(user) {
+            return [new Trip(), new Trip()]
+        }
+    }
+    const me = new User()
     it('should_Not_Return_Trips_When_Logged_User_Are_Not_Friend', () => {
+        UserSession.getLoggedUser = () => me
         class TestableUser extends User {
             getFriends() {
                 return []
             }
         }
         const targetUser = new TestableUser()
-        const me = new User()
-        const UserSession = {
-            getLoggedUser() {
-                return me
-            }
-        }
-        const TripDAO = {
-            findTripsByUser(user) {
-                return [new Trip(), new Trip()]
-            }
-        }
-        const tripService = new TripService()
         const trips = tripService.getTripsByUser(targetUser, UserSession, TripDAO).length
         assert.equal(trips, 0);
     });
 
     it('should_Return_Trips_When_Logged_User_Are_Friend', () => {
-        class Trip { }
+        UserSession.getLoggedUser = () => me
         class TestableUser extends User {
             getFriends() {
                 return [me]
             }
         }
         const targetUser = new TestableUser()
-        const me = new User()
-        const UserSession = {
-            getLoggedUser() {
-                return me
-            }
-        }
-        const TripDAO = {
-            findTripsByUser(user) {
-                return [new Trip(), new Trip()]
-            }
-        }
-        const tripService = new TripService()
         const trips = tripService.getTripsByUser(targetUser, UserSession, TripDAO).length
         assert.equal(trips, 2);
     });
